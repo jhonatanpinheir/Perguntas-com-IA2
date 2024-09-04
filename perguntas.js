4c9f547bd7e5a28e89074179e6fb5aad11560377
jhonatanpinheir <134715337+jhonatanpinheir@users.noreply.github.com>
2024-09-04 08:18:58 -0300

    stop perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,19 @@
 export const perguntas = [
     {
-        enunciado: " ",
+        enunciado: "Um dia você acorda e descobre que a tecnologia agora pode te responder tudo, literalmente tudo que você quiser saber, qual seria sua reação?",
         alternativas: [
             {
-                texto: "",
+                texto: "Uau, isso é impressionante!",
                 afirmacao: [
-                    "",
+                    "Agora que posso obter respostas para qualquer pergunta.",
                     ""
                 ],
                 proxima: 1,
             },
             {
-                texto: "",
+                texto: "Isso é preocupante.",
                 afirmacao: [
-                    "",
+                    "Ter todas as respostas pode fazer com que eu dependa demais da tecnologia.",
                     ""
                 ],
                 proxima: 2,
@@ -21,21 +21,21 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Com a possibilidade de agora saber oque quiser qual atitude você toma?",
         alternativas: [
             {
-                texto: "",
+                texto: "Com a possibilidade de saber o que quiser, vou me concentrar em fazer perguntas que me ajudem a crescer e a entender o mundo de forma mais profunda, em vez de apenas buscar respostas rápidas.",
                 afirmacao: [
-                    "",
+                    "Me concentro e busco por informações que vão agregar conhecimento a mim.",
                     "",
                     ""
                 ],
                 proxima: 3,
             },
             {
-                texto: "",
+                texto: "Com a possibilidade de saber o que quiser, posso acabar me sentindo sobrecarregado e me tornar dependente da tecnologia.",
                 afirmacao: [
-                    "",
+                    "Deixo isso de lado por enquanto.",
                     "",
                     ""
                 ],
@@ -44,21 +44,21 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Em meu dia a dia surge uma duvida muito difícil, e tiro minha duvida com a tecnologia.",
         alternativas: [
             {
-                texto: "",
+                texto: "Faço a pergunta para a tecnologia e ela me responde muito bem.",
                 afirmacao: [
-                    "",
+                    "Ela me respondeu detalhe por detalhe muito bem explicado.",
                     "",
                     ""
                 ],
                 proxima: 3,
             },
             {
-                texto: "",
+                texto: "Acabo fazendo a pergunta para a nova tecnologia e ela me responde.",
                 afirmacao: [
-                    "",
+                    "Não sei se devo confiar muito nessa tecnologia nova, vou fazer mais pesquisas para saber se ela acertou.",
                     "",
                     ""
                 ],
@@ -67,20 +67,20 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "",
+        enunciado: "A tecnologia nova faz muito sucesso com todos.",
         alternativas: [
             {
-                texto: "",
+                texto: "Todos usam da tegnologia e agradecem seu criador.",
                 afirmacao: [
-                    "",
+                    "Ela ajuda em todas as aréas de trabalho.",
                     ""
                 ],
                 proxima: 5,
             },
             {
-                texto: "",
+                texto: "Todos usam dessa tecnologia e isso pode ser um problema.",
                 afirmacao: [
-                    "",
+                    "Ela pode tirar vários empregos como professores, contadores, administradores,e etc.",
                     ""
                 ],
                 proxima: 6,
@@ -88,20 +88,20 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Essa tecnologia pode ser um problema grave?",
         alternativas: [
             {
-                texto: "",
+                texto: "Ela apenas ajuda a obter mais conhecimento respondendo qualquer de suas duvidas.",
                 afirmacao: [
-                    "",
+                    "A melhor coisa inventada até agora pois pode ajudar a todos.",
                     ""
                 ],
                 proxima: 5,
             },
             {
-                texto: "",
+                texto: "Pode ser boa, mais esta causando a demissão de algumas aréas de trbalho.",
                 afirmacao: [
-                    "",
+                    "Ela esta causando a demissão de professores, pois não tem mais oque se aprender pois ela responde tudo que você precisar.",
                     ""
                 ],
                 proxima: 6,
